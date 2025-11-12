0420dfb9fdb0343ae14f7891c97f82e2dbc41518
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-11-12 11:37:57 +0200

    Create ZodiacModel class in model

---
+++ ossp_zodiac_mvc_model.py
@@ -0,0 +1,27 @@
+class ZodiacModel:
+    @staticmethod
+    def zodiac_for(day: int, month: int):
+        if (month == 1 and day >= 20) or (month == 2 and day <= 18):
+            return "Водолей"
+        elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+            return "Рыбы"
+        elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
+            return "Овен"
+        elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+            return = "Телец"
+        elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+            return "Близнецы"
+        elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
+            return "Рак"
+        elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
+            return "Лев"
+        elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+            return "Дева"
+        elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
+            return "Весы"
+        elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+            return "Скорпион"
+        elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+            return "Стрелец"
+        else:
+            return "Козерог"
